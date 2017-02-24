0ab9b5e48cd7b245286ab27d48183b0621f7589b
储诚鹏 <chuchengpeng0119@163.com>
2017-02-24 10:11:44 +0800

    Initial Commit

---
+++ CCPRealM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPRealM
+//
+//  Created by 储诚鹏 on 17/2/24.
+//  Copyright © 2017年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCPRealM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPRealM
+//
+//  Created by 储诚鹏 on 17/2/24.
+//  Copyright © 2017年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
