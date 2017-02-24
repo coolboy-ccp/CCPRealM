beaac526fde6aaad23df9c852a1ebc911de912d2
储诚鹏 <chuchengpeng0119@163.com>
2017-02-24 11:07:35 +0800

    my realm
    my realm

--- CCPRealM/ViewController.m
+++ CCPRealM/ViewController.m
@@ -7,6 +7,28 @@
 //
 
 #import "ViewController.h"
+#import "Realm.h"
+
+@interface Dog : RLMObject
+@property NSString *name;
+@property NSInteger age;
+@end
+
+@implementation Dog
+
+@end
+
+RLM_ARRAY_TYPE(Dog);
+@interface Person : RLMObject
+
+@property NSString *name;
+@property RLMArray<Dog *><Dog> *dogs;
+
+@end
+
+@implementation Person
+
+@end
 
 @interface ViewController ()
 
@@ -16,6 +38,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    Dog *dog = [[Dog alloc] init];
+    dog.name = @"honey";
+    dog.age = 1;
+    Person *person = [[Person alloc] init];
+    person.name = @"mick";
+    [person.dogs addObject:dog];
+    RLMResults *result = [Dog objectsWhere:@"age < 2"];
+    NSLog(@"result:%@",result);
+    [[RLMRealm defaultRealm] transactionWithBlock:^{
+        [[RLMRealm defaultRealm] addObject:dog];
+        NSLog(@"result2:%@",result);
+    }];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
